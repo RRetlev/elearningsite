5eb56c4a4d6e49889ffc067626bd6144c1c37744
lupakd <daniel.lupis@gmail.com>
2019-08-22 09:26:35 +0200

    get sends question data, add question to mem working.

--- Backend/src/main/java/com/codecool/elearning/controller/QuestionController.java
+++ Backend/src/main/java/com/codecool/elearning/controller/QuestionController.java
@@ -7,8 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@CrossOrigin
 @RestController
 public class QuestionController {
 
--- Backend/src/main/java/com/codecool/elearning/model/Question.java
+++ Backend/src/main/java/com/codecool/elearning/model/Question.java
@@ -2,33 +2,32 @@ package com.codecool.elearning.model;
 
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 
 public class Question {
 
     private String question;
+    private List<Answer> answers = new ArrayList<>();
 
     public String getQuestion() {
         return question;
     }
 
-    private Map<String, Boolean> answers = new HashMap<>();
-
-    public Map<String, Boolean> getAnswers() {
+    public List<Answer> getAnswers() {
         return answers;
     }
 
-    public Question(String question, String a1, String a2, String a3, String a4) {
+    public Question(String question, String... answerTexts) {
+        boolean firstTrue = true;
         this.question = question;
-        addAnswers(a1, a2, a3, a4);
+        for (String answer : answerTexts) {
+            this.answers.add(new Answer(firstTrue, answer));
+            firstTrue = false;
+        }
     }
 
-    public void addAnswers(String a1, String a2, String a3, String a4) {
-        answers.put(a1, true);
-        answers.put(a2, false);
-        answers.put(a3, false);
-        answers.put(a4, false);
-    }
 }

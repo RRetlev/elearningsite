b138f7252b745256b557b1556198bd04d399a40b
Retlev <rretlev@gmail.com>
2019-08-22 08:44:39 +0200

    frontend layout

--- Backend/src/main/java/com/codecool/elearning/controller/QuestionController.java
+++ Backend/src/main/java/com/codecool/elearning/controller/QuestionController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 public class QuestionController {
 
--- Frontend/elearningsite/src/App.js
+++ Frontend/elearningsite/src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from './components/container';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    question: null,
+    answers: {}
+  }
+  componentDidMount(){
+    console.log("DiD Mount")
+    fetch('http://localhost:8080/question')
+    .then(response => response.json())
+    .then(data => 
+      this.setState({answers: data.answers,
+      question: data.question})
+      )
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <Container
+         question={this.state.question}
+         answers={this.state.answers}/>
+      </div>
+    );
+  }
 }
 
 export default App;

14d5197e38110865a3b61d82368fa96bbc29e60a
Gergely Simonics <gergelysimonics@Gergelys-MacBook-Pro.local>
2019-09-06 00:32:14 +0200

    made the question asking page design fancy

--- Frontend/elearningsite/src/components/pages/newquestion.js
+++ Frontend/elearningsite/src/components/pages/newquestion.js
@@ -1,99 +1,105 @@
 import React, {Component} from 'react'
 
-export class AddQuestion extends Component{
-    state={
-        question:' ',
-        trueanswer:' ',
-        false1:' ',
-        false2:' ',
-        false3:' ',
+export class AddQuestion extends Component {
+    state = {
+        question: ' ',
+        trueanswer: ' ',
+        false1: ' ',
+        false2: ' ',
+        false3: ' ',
     };
 
-    onSubmit=(e) =>{
-        const result={
-            question:this.state.question,
-            answers:[
+    onSubmit = (e) => {
+        const result = {
+            question: this.state.question,
+            answers: [
                 {
-                    right:true,
-                    text:this.state.trueanswer,
+                    right: true,
+                    text: this.state.trueanswer,
                 },
                 {
-                    right:false,
-                    text:this.state.false1,
+                    right: false,
+                    text: this.state.false1,
                 },
                 {
-                    right:true,
-                    text:this.state.false2,
+                    right: true,
+                    text: this.state.false2,
                 },
                 {
-                    right:true,
-                    text:this.state.false3,
+                    right: true,
+                    text: this.state.false3,
                 }
             ]
-        }
+        };
+
         fetch('http://localhost:8080/new-question', {
             method: 'POST',
+            mode: 'cors',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             },
             body: JSON.stringify(result)
-        });
-        this.setState({question:' ', trueanswer:' ',false1:' ',false2:' ',false3:' ',})
+
+        })
+            .then(res => console.log(res));
+        this.setState({question: ' ', trueanswer: ' ', false1: ' ', false2: ' ', false3: ' ',})
     };
 
-    onChange =(e) => this.setState({[e.target.name]:e.target.value});
+    onChange = (e) => this.setState({[e.target.name]: e.target.value});
 
-    render(){
-        return(
-            <form onSubmit={this.onSubmit} style={{display:'flex'}}>
-            <input
-                type='text'
-                name='question'
-                onChange={this.onChange}
-                style={{flex:'10', padding:'5px'}}
-                placeholder='Question'
+    render() {
+        return (
+            <form onSubmit={this.onSubmit} /*style={{display:'flex'}}*/>
 
-            />
-            <input
-                type='text'
-                name='trueanswer'
-                onChange={this.onChange}
-                style={{flex:'10', padding:'5px'}}
-                placeholder='True answer'
+                <div className="input-field-wrapper">
+                    <input required
+                        type='text'
+                        name='question'
+                        onChange={this.onChange}
+                        // style={{flex:'10', padding:'5px'}}
+                        placeholder='Question'
 
-            />
-            <input
-                type='text'
-                name='false1'
-                onChange={this.onChange}
-                style={{flex:'10', padding:'5px'}}
-                placeholder='First false answer'
+                    />
+                    <input required
+                        type='text'
+                        name='trueanswer'
+                        onChange={this.onChange}
+                        // style={{flex:'10', padding:'5px'}}
+                        placeholder='True answer'
 
-            />
-            <input
-                type='text'
-                name='false2'
-                onChange={this.onChange}
-                style={{flex:'10', padding:'5px'}}
-                placeholder='Second false answer'
+                    />
+                    <input required
+                        type='text'
+                        name='false1'
+                        onChange={this.onChange}
+                        // style={{flex:'10', padding:'5px'}}
+                        placeholder='First false answer'
 
-            />
-            <input
-                type='text'
-                name='false3'
-                onChange={this.onChange}
-                style={{flex:'10', padding:'5px'}}
-                placeholder='Third false answer'
+                    />
+                    <input required
+                        type='text'
+                        name='false2'
+                        onChange={this.onChange}
+                        // style={{flex: '10', padding: '5px'}}
+                        placeholder='Second false answer'
 
-            />
-            <input
-                type='submit'
-                value='Submit'
-                className='btn'
-                style={{flex:1}}
-            />
-        </form>
+                    />
+                    <input required
+                        type='text'
+                        name='false3'
+                        onChange={this.onChange}
+                        // style={{flex:'10', padding:'5px'}}
+                        placeholder='Third false answer'
+                    />
+                </div>
+                <input
+                    type='submit'
+                    value='Submit'
+                    className='submit-btn'
+                    // style={{flex:1}}
+                />
+            </form>
 
 
         )

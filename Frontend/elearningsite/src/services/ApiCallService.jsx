c39794b1e21517f53eb3c2764726380c85544f88
Retlev <rretlev@gmail.com>
2019-09-18 15:53:13 +0200

    Bug fix on the add-question route.

--- Frontend/elearningsite/src/services/ApiCallService.jsx
+++ Frontend/elearningsite/src/services/ApiCallService.jsx
@@ -4,4 +4,9 @@ export function fetchQuestion() {
         .then(response => response.json());
 }
 
+export function fetchUser(){
+    return fetch('http://localhost:8080/auth/signin')
+        .then(response => response.json()) 
+}
+
 
